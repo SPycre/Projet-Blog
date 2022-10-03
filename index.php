c373661b40ee2dde4e5f65296e102cef3e891752
SPycre <67427143+SPycre@users.noreply.github.com>
2022-10-03 10:36:47 +0200

    Ajout index.php & header.html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <header id="header">
+            <?php include("header.html"); ?>
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
