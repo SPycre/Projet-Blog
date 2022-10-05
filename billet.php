650a2804dced1f02fabfe0a3aa3c009d3f4d67e2
Zatosee <Steve.Saissac07@gmail.com>
2022-10-05 09:08:33 +0200

    Footer creation

---
+++ billet.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title> Création de billet </title>
+        <link href="CSS/header.css" rel="stylesheet"/>
+        <link href="CSS/main.css" rel="stylesheet"/>
+        <link href="CSS/footer.css" rel="stylesheet"/>
+
+    </head>
+    <body>
+        <header id="header">
+        <!-- ?php include("header.html"); ?--> 
+        </header>
+
+
+        <main>
+            <div id = "Billet">
+                    
+                </div>
+        </main>
+
+
+        <footer>
+        <?php include("footer.html"); ?>
+        </footer>
+    </body>
+</html>
