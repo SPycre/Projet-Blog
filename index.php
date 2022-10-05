528ce5b9800c0e7d9c1397be77379220e620f3bd
SPycre <67427143+SPycre@users.noreply.github.com>
2022-10-05 09:10:22 +0200

    Ajout header.html + avancement index.php

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 <html>
     <head>
         <title></title>
+        <link href="CSS/header.css" rel="stylesheet"/>
+        <link href="CSS/home.css" rel="stylesheet"/>
+        <link href="CSS/main.css" rel="stylesheet"/>
     </head>
     <body>
         <header id="header">
             <?php include("header.html"); ?>
         </header>
         <main>
-
+            <section id="liste-billets">
+                <article class="billet">
+                    <div class="text-billet">
+                        <span class="titre-billet">Titre</span>
+                        <span class="contenu-billet">Content</span>
+                    </div>
+                    <div class="detail-billet">
+                        
+                    </div>
+                </article>
+            </section>
         </main>
         <footer>
 
