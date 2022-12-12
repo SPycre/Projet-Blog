ac54a33ed7f9f73079ac3e226370c071b9698868
Zatosee <99033119+Zatosee@users.noreply.github.com>
2022-12-12 15:24:23 +0100

    Creation mention and politic (2 pages)
    Commit to steve

---
+++ politique.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Projet de Blog</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="module" src="Client/billet.js" defer></script>
+
+    <link href="CSS/header.css" rel="stylesheet"/>
+    <link href="CSS/main.css" rel="stylesheet"/>
+    <link href="CSS/footer.css" rel="stylesheet"/>
+    <link href="CSS/billet.css" rel="stylesheet"/>
+    <link href="CSS/mention.css" rel="stylesheet"/>
+
+
+</head>
+<body>
+<header style="position:fixed;right:0;left:0;top:0;z-index: 1000">
+    <?php include("header.html"); ?>
+</header>
+
+
+<main>
+    <div class="PageMention">
+
+        <h1> Politique de confidentialité </h1>
+
+        <p>
+            <strong> 1. Introduction </strong>
+            </br></br>
+        La confidentialité des visiteurs de notre site web est très importante à nos yeux, et nous nous engageons à la protéger. Cette politique détaille ce que nous faisons de vos informations personnelles. Consentir à notre utilisation de cookies en accord avec cette politique lors de votre première visite de notre site web nous permet d’utiliser des cookies à chaque fois que vous consultez notre site.
+        </p>
+
+        <p>
+            <strong> 2. Source </strong>
+            </br></br>
+        Ce document a été créé grâce à un modèle de SEQ Legal (seqlegal.com)
+        </p>
+
+        <p>
+            <strong> 3. Collecte d’informations personnelles </strong>
+            </br></br>
+            Les types d’informations personnelles suivants peuvent collectés, stockés et utilisés : </br>
+        </p>
+
+        <p>
+        <ul>
+        <li> Des informations à propos de votre ordinateur, y compris votre adresse IP, votre localisation géographique, le type et la version de votre navigateur, et votre système d’exploitation. </br></li>
+        <li> Des informations sur vos visites et votre utilisation de ce site web y compris la source référente, la durée de la visite, les pages vues, et les chemins de navigation de sites web. </br></li>
+        <li> Des informations comme votre nom et votre adresse e-mail, que vous saisissez pour souscrire à nos e-mails et/ou newsletters. </br></li>
+        <li> Des informations que vous saisissez quand vous utilisez les services de notre site web.</br></li>
+        <li> Des informations générées lors de l’utilisation de notre site, y compris quand, à quelle fréquence et sous quelles circonstances vous l’utilisez. </br></li>
+        <li> Des informations que vous publiez sur notre site web dans l’intention de les publier sur internet, et qui incluent votre identifiant, vos photos de profil et le contenu de vos publications </br></li>
+        <li> Avant de nous divulguer des informations personnelles concernant une autre personne, vous devez obtenir le consentement de ladite personne en ce qui concerne la divulgation et le traitement de ces informations personnelles selon les termes de cette politique. </li>
+        </ul>
+        </p>
+
+
+        <p>
+            <strong> 4. Utilisation de vos informations personnelles </strong>
+        </br></br>
+
+        Les informations personnelles qui nous sont fournies par le biais de notre site web seront utilisées dans les objectifs décrits dans cette politique ou dans les pages du site pertinentes. Nous pouvons utiliser vos informations personnelles pour :
+            </br></br>
+        Administrer notre site web et notre entreprise </br>
+        Personnaliser notre site web pour vous </br>
+        Maintenir la sécurité de notre site web et empêcher la fraude </br>
+        Vérifier le respect des conditions générales qui régissent l’utilisation de notre site web (y compris surveiller les messages privés envoyés par le biais du service de messagerie privé de notre site web) </br>
+        Si vous soumettez des informations personnelles sur notre site web dans le but de les publier, nous les publierons et pourrons utiliser ces informations conformément aux autorisations que vous nous accordez.</br></br>
+
+        Sans votre consentement explicite, nous ne fournirons pas vos informations personnelles à des tierces parties pour leur marketing direct, ni celui d’autres tierces parties.
+
+        Quand vous laissez un commentaire sur notre site web, les données inscrites dans le formulaire de commentaire, mais aussi votre adresse IP et l’agent utilisateur de votre navigateur sont collectés pour nous aider à la détection des commentaires indésirables.
+
+        Une chaîne anonymisée créée à partir de votre adresse de messagerie (également appelée hash) peut être envoyée au service Gravatar pour vérifier si vous utilisez ce dernier. Les clauses de confidentialité du service Gravatar sont disponibles ici : https://automattic.com/privacy/. Après validation de votre commentaire, votre photo de profil sera visible publiquement à coté de votre commentaire.
+
+        Si vous êtes un utilisateur ou une utilisatrice enregistré·e et que vous téléversez des images sur le site web, nous vous conseillons d’éviter de téléverser des images contenant des données EXIF de coordonnées GPS. Les visiteurs de votre site web peuvent télécharger et extraire des données de localisation depuis ces images.
+
+        </p>
+
+        <p>
+            <strong> 5. Divulgation de vos informations personnelles </strong>
+            </br></br>
+
+        Nous pouvons divulguer vos informations personnelles à n’importe lequel de nos employés, dirigeants, assureurs, conseillers professionnels, agents, fournisseurs, ou sous-traitants dans la mesure où cela est raisonnablement nécessaire aux fins énoncées dans cette politique.
+
+        Nous pouvons divulguer vos informations personnelles à n’importe quel membre de notre groupe d’entreprises (cela signifie nos filiales, notre société holding ultime et toutes ses filiales) dans la mesure où cela est raisonnablement nécessaire aux fins énoncées dans cette politique.
+            </br></br>
+            Nous pouvons divulguer vos informations personnelles : </br></br>
+
+        Dans la mesure où nous sommes tenus de le faire par la loi </br>
+        Dans le cadre de toute procédure judiciaire en cours ou à venir </br>
+        Pour établir, exercer ou défendre nos droits légaux (y compris fournir des informations à d’autres à des fins de prévention des fraudes et de réduction des risques de crédit) </br>
+        À l’acheteur (ou acheteur potentiel) de toute entreprise ou actif en notre possession que nous souhaitons (ou envisageons de) vendre </br>
+        À toute personne que nous estimons raisonnablement faire partie intégrante d’un tribunal ou autre autorité compétente pour la divulgation de ces informations personnelles si, selon notre opinion, un tel tribunal ou une telle autorité serait susceptible de demander la divulgation de ces informations personnelles. </br>
+        Sauf disposition contraire de la présente politique, nous ne transmettrons pas vos informations personnelles à des tierces parties. </br>
+
+        </p>
+
+        <p>
+            <strong> 6. Transferts internationaux de données </strong>
+            </br></br>
+
+        Les informations que nous collectons peuvent être stockées, traitées et transférées dans tous les pays dans lesquels nous opérons, afin de nous permettre d’utiliser les informations en accord avec cette politique.
+
+        Les informations que nous collectons peuvent être transférées dans les pays suivants, n’ayant pas de lois de protections des données équivalentes à celles en vigueur dans l’espace économique européen : les États-Unis d’Amérique, la Russie, le Japon, la Chine et l’Inde.
+
+        Les informations personnelles que vous publiez sur notre site web ou que vous soumettez à la publication peuvent être disponibles, via internet, dans le monde entier. Nous ne pouvons empêcher l’utilisation, bonne ou mauvaise, de ces informations par des tiers.
+
+        Vous acceptez expressément le transfert d’informations personnelles décrit dans cette Section 6.
+            </p>
+
+        <p>
+            <strong> 7. Conservation de vos informations personnelles </strong>
+            </br></br>
+        Cette Section 7. détaille nos politiques de conservation des données et nos procédures, conçues pour nous aider à nous conformer à nos obligations légales concernant la conservation et la suppression d’informations personnelles. </br>
+
+        Les informations personnelles que nous traitons à quelque fin que ce soit ne sont pas conservées plus longtemps que nécessaire à cette fin ou à ces fins. </br></br>
+
+        Sans préjudice à l’article, nous supprimerons généralement les données personnelles de ces catégories : </br></br>
+
+        La durée de conservation des données : 3 ans </br>
+        Nous conserverons des documents (y compris des documents électroniques) contenant des données personnelles : </br>
+
+        Dans la mesure où nous sommes tenus de le faire par la loi </br>
+        Si nous pensons que les documents peuvent être pertinents pour toute procédure judiciaire en cours ou potentielle </br>
+
+        Pour établir, exercer ou défendre nos droits légaux (y compris fournir des informations à d’autres à des fins de préventi on des fraudes et de réduction des risques de crédit).
+        Si vous laissez un commentaire, le commentaire et ses métadonnées sont conservés indéfiniment. Cela permet de reconnaître et approuver automatiquement les commentaires suivants au lieu de les laisser dans la file de modération.
+
+            </br>Pour les utilisateurs et utilisatrices qui s’inscrivent sur notre site (si cela est possible), nous stockons également les données personnelles indiquées dans leur profil. </br>Tous les utilisateurs et utilisatrices peuvent voir, modifier ou supprimer leurs informations personnelles à tout moment (à l’exception de leur nom d’utilisateur·ice). Les gestionnaires du site peuvent aussi voir et modifier ces informations.
+        </p>
+
+        <p>
+            <strong>  8. Sécurité de vos informations personnelles </strong>
+            </br></br>
+
+        Nous prendrons des précautions techniques et organisationnelles raisonnables pour empêcher la perte, l’abus ou l’altération de vos informations personnelle.
+
+        Nous stockerons toutes les informations personnelles que vous nous fournissez sur des serveurs sécurisés (protégés par mot de passe et pare-feu).
+
+        Toutes les transactions financières électroniques effectuées par le biais de notre site web seront protégées par des technologies de cryptage.
+
+        Vous reconnaissez que la transmission d’informations par internet est intrinsèquement non sécurisée, et que nous ne pouvons pas garantir la sécurité de vos données envoyées par internet.
+
+        Vous êtes responsable de la confidentialité du mot de passe que vous utilisez pour accéder à notre site web ; nous ne vous demanderons pas votre mot de passe (sauf quand vous vous identifiez sur notre site web).
+        </p>
+
+        <p>
+            <strong> 9. Amendements </strong>
+            </br></br>
+        Nous pouvons parfois mettre cette politique à jour en publiant une nouvelle version sur notre site web. Vous devez vérifier cette page régulièrement pour vous assurer de prendre connaissance de tout changement effectué à cette politique. Nous pouvons vous informer des changements effectués à cette politique par courrier électronique ou par le biais du service de messagerie privée de notre site web.
+        </p>
+
+        <p>
+            <strong> 10. Vos droits </strong>
+            </br></br>
+        Vous pouvez nous demander de vous fournir toute information personnelle que nous détenons sur vous ; le transfert de telles informations sera soumis aux conditions suivantes :
+
+        Nous pouvons retenir les informations personnelles que vous demandez dans la mesure autorisée par la loi.
+        Vous pouvez nous demander à tout moment de ne pas traiter vos informations personnelles à des fins marketing.
+        En pratique, vous exprimerez expressément et à l’avance votre accord pour que nous utilisions vos informations personnelles à des fins marketing ou nous vous fournirons une opportunité de refuser l’utilisation de vos informations personnelles à des fins marketing.
+        Si vous avez un compte ou si vous avez laissé des commentaires sur le site, vous pouvez demander à recevoir un fichier contenant toutes les données personnelles que nous possédons à votre sujet, incluant celles que vous nous avez fournies. Vous pouvez également demander la suppression des données personnelles vous concernant. Cela ne prend pas en compte les données stockées à des fins administratives, légales ou pour des raisons de sécurité.
+        </p>
+
+        <br>
+            <strong> 13. Cookies </strong>
+            </br></br>
+        Notre site web utilise des cookies. Un cookie est un fichier contenant un identifiant (une chaîne de lettres et de chiffres) envoyé par un serveur web vers un navigateur web et stocké par le navigateur. L’identifiant est alors renvoyé au serveur à chaque fois que le navigateur demande une page au serveur. Les cookies peuvent être « persistants » ou « de session » : un cookie persistant est stocké par le navigateur et reste valide jusqu’à sa date d’expiration, à moins d’être supprimé par l’utilisateur avant cette date d’expiration ; quant à un cookie de session, il expire à la fin de la session utilisateur, lors de la fermeture du navigateur. Les cookies ne contiennent en général aucune information permettant d’identifier personnellement un utilisateur, mais les informations personnelles que nous stockons à votre sujet peuvent être liées aux informations stockées dans les cookies et obtenues par les cookies.
+            Nous n’utilisons que des cookies de session et des cookies persistants sur notre site web</br>
+
+        Les noms des cookies que nous utilisons sur notre site web et les objectifs dans lesquels nous les utilisons sont décrits ci-dessous :</br></br>
+
+        Nous pouvons utiliser les cookies pour :</br></br>
+
+        Reconnaître un ordinateur lorsqu’un utilisateur consulte le site web</br>
+        Suivre les utilisateurs lors de leur navigation sur le site web</br>
+        Activer l’utilisation d’un panier sur le site web</br>
+        Améliorer l’utilisation d’un site web</br>
+        Analyser l’utilisation du site web</br>
+        Administrer le site web</br>
+        Empêcher la fraude et améliorer la sécurité du site web</br>
+        Personnaliser le site web pour chaque utilisateur</br>
+        Envoyer des publicités ciblées pouvant intéresser certains utilisateurs</br></br>
+
+        La plupart des navigateurs vous permettent de refuser ou d’accepter les cookies :</br></br>
+
+        <ul>
+        <li>Avec Internet Explorer (version 10), vous pouvez bloquer les cookies en utilisant les paramètres de remplacement de la gestion des cookies disponibles en cliquant sur «Outils», «Options internet», «Confidentialité» puis «Avancé»</br></li>
+        <li>Avec Firefox (version 24), vous pouvez bloquer tous les cookies en cliquant sur «Outils», «Options», «Confidentialité» puis en sélectionnant «Utiliser des paramètres personnalisés pour l’historique» depuis le menu déroulant et en décochant «Accepter les cookies provenant des sites»</br></li>
+        <li>Avec Chrome (version 29), vous pouvez bloquer tous les cookies en accédant au menu «Personnaliser et contrôler» puis en cliquant sur «Paramètres», «Montrer les paramètres avancés» et «Paramètres de contenu» puis en sélectionnant «Empêcher les sites de définir des données» dans l’en-tête «Cookies».</br></li>
+        <li>Bloquer tous les cookies aura un impact négatif sur l’utilisation de nombreux sites web. Si vous bloquez les cookies, vous ne pourrez pas utiliser toutes les fonctionnalités de notre site web.</br></br></li>
+        </ul>
+
+        Vous pouvez supprimer les cookies déjà stockés sur votre ordinateur :</br></br>
+
+        <ul>
+        <li>Avec Internet Explorer (version 10), vous devez supprimer le fichier cookies manuellement (vous pourrez trouver des instructions pour le faire ici )</br></li>
+        <li>Avec Firefox (version 24), vous pouvez supprimer les cookies en cliquant sur «Outils», «Options», et «Confidentialité», puis en sélectionnant «Utiliser des paramètres personnalisés pour l’historique» et en cliquant sur «Montrer les cookies», puis sur «Supprimer tous les cookies»</br></li>
+        <li>Avec Chrome (version 29), vous pouvez supprimer tous les cookies en accédant au menu «Personnaliser et contrôler» puis en cliquant sur «Paramètres», « Montrer les paramètres avancés » et «Supprimer les données de navigation» puis «Supprimer les cookies et les données des modules d’autres sites» avant de cliquer sur «Supprimer les données de navigation».</br></li>
+        <li>Supprimer les cookies aura un impact négatif sur l’utilisation de nombreux sites web.</br></br></li>
+        </ul>
+            
+
+        Si vous vous rendez sur la page de connexion, un cookie temporaire sera créé afin de déterminer si votre navigateur accepte les cookies. Il ne contient pas de données personnelles et sera supprimé automatiquement à la fermeture de votre navigateur.
+        Lorsque vous vous connecterez, nous mettrons en place un certain nombre de cookies pour enregistrer vos informations de connexion et vos préférences d’écran. La durée de vie d’un cookie de connexion est de deux jours, celle d’un cookie d’option d’écran est d’un an. </br>Si vous cochez « Se souvenir de moi », votre cookie de connexion sera conservé pendant deux semaines. Si vous vous déconnectez de votre compte, le cookie de connexion sera effacé.
+
+        En modifiant ou en publiant une publication, un cookie supplémentaire sera enregistré dans votre navigateur. Ce cookie ne comprend aucune donnée personnelle. Il indique simplement l’ID de la publication que vous venez de modifier. Il expire au bout d’un jour.
+        </p>
+
+    </div>
+</main>
+
+<footer style="position:fixed">
+    <?php include("footer.html"); ?>
+</footer>
+
+</body>
+</html>
