2a50d3bdda43460aa899c9bf2429b58f7bdff23d
Steve <Steve.Saissac07@gmail.com>
2023-01-04 15:22:36 +0100

    Fix menu burger login

--- login.php
+++ login.php
@@ -4,15 +4,21 @@
 	<title>Log in</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
-
 	<script type="module" src="Client/login.js" defer></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+	<link href="css/login.css" rel="stylesheet"/>
+	<link href="CSS/header.css" rel="stylesheet"/>
+	<link href="CSS/main.css" rel="stylesheet"/>
+	<link href="CSS/footer.css" rel="stylesheet"/>
 </head>
 <body>
+	<header>
+        <?php include("header.html"); ?>
+    </header>
 	<main>
 		<section id="central">
-			<div id="login-box">
+			<div id="login-box" class="section-box">
 				<form id="login-form">
 
 						<span id="login-form-title">
@@ -39,5 +45,8 @@
 			</div>
 		</section>
 	</main>
+	<footer>
+        <?php include("footer.html"); ?>
+    </footer>
 </body>
 </html>
